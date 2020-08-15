a694114a15717e454b7fd89492550cb4f17764b6
Xiaoyi Liu <lxy878@gmail.com>
2020-08-15 12:48:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateDriverWithKeyAndValue(obj, key, value){
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(obj, key, value){
-  return Object.assign(obj, {[key]: value})
+  return Object.assign(obj, {[key]: value});
 }
 
 function deleteFromDriverByKey(obj, key){
