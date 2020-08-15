db1072c39c0c608fa0e80bf3f91436cba044b877
Xiaoyi Liu <lxy878@gmail.com>
2020-08-15 12:52:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ function destructivelyUpdateDriverWithKeyAndValue(obj, key, value){
 }
 
 function deleteFromDriverByKey(obj, key){
-  delete obj[key];
+  const newObj = Object.assign({}, obj);
+  delete newObj[key];
+  return newObj;
 }
